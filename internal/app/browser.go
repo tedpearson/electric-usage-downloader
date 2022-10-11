9be19bda2758acf9ef82b5e8d5dae8c7470cf446
Ted Pearson <ted@tedpearson.com>
2022-10-11 01:52:15 -0400

    run chrome headless

--- internal/app/browser.go
+++ internal/app/browser.go
@@ -11,10 +11,7 @@ import (
 )
 
 func DownloadCsv(config *Config, startDate string, endDate string) (string, error) {
-	allocatorFlags := append(chromedp.DefaultExecAllocatorOptions[:], chromedp.Flag("headless", false))
-	ctx, cancel := chromedp.NewExecAllocator(context.Background(), allocatorFlags...)
-	defer cancel()
-	ctx, cancel = chromedp.NewContext(ctx, chromedp.WithLogf(log.Printf))
+	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithLogf(log.Printf))
 	defer cancel()
 	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
